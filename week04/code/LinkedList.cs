1b00013396abcafb71bd886cf08a1a29bd70a692
haithcock <dakota.haith1997@.com>
2025-09-28 12:47:56 -0400

    week 4 done

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -33,6 +33,19 @@ public class LinkedList : IEnumerable<int>
     public void InsertTail(int value)
     {
         // TODO Problem 1
+        Node newNode = new(value);
+
+        if (_head is null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            newNode.Prev = _tail;
+            _tail!.Next = newNode;
+            _tail = newNode;
+        }
     }
 
 
@@ -65,6 +78,16 @@ public class LinkedList : IEnumerable<int>
     public void RemoveTail()
     {
         // TODO Problem 2
+        if (_head == _tail)
+        {
+            _head = null;
+            _tail = null;
+        }
+        else if (_tail is not null)
+        {
+            _tail.Prev!.Next = null;
+            _tail = _tail.Prev;
+        }
     }
 
     /// <summary>
@@ -109,7 +132,49 @@ public class LinkedList : IEnumerable<int>
     public void Remove(int value)
     {
         // TODO Problem 3
+        if (_head == null)
+        {
+            return;
+        }
+
+        if (_head!.Data == value)
+        {
+            if (_head == _tail)
+            {
+                _head = null;
+                _tail = null;
+            }
+            else
+            {
+                _head = _head.Next;
+                _head!.Prev = null;
+            }
+            return;
+        }
+
+        Node? curr = _head;
+        while (curr.Next != null && curr.Next.Data != value)
+        {
+            curr = curr.Next;
+        }
+
+        if (curr.Next != null)
+        {
+            Node? nodeToRemove = curr.Next;
+
+            if (nodeToRemove == _tail)
+            {
+                _tail = curr; // The node before the removed tail becomes the new tail
+                _tail.Next = null; // New tail's next should be null
+            }
+            else //
+            {
+                curr.Next = nodeToRemove.Next; // Link current node's Next to the node after the one to be removed
+                nodeToRemove.Next!.Prev = curr; 
+            }
+        }
     }
+
 
     /// <summary>
     /// Search for all instances of 'oldValue' and replace the value to 'newValue'.
@@ -117,6 +182,16 @@ public class LinkedList : IEnumerable<int>
     public void Replace(int oldValue, int newValue)
     {
         // TODO Problem 4
+        Node? curr = _head;
+
+        while (curr != null)
+        {
+            if (curr.Data == oldValue)
+            {
+                curr.Data = newValue;
+            }
+            curr = curr.Next;
+        }
     }
 
     /// <summary>
@@ -147,7 +222,13 @@ public class LinkedList : IEnumerable<int>
     public IEnumerable Reverse()
     {
         // TODO Problem 5
-        yield return 0; // replace this line with the correct yield return statement(s)
+        var curr = _tail;
+
+        while (curr is not null)
+        {
+            yield return curr.Data;
+            curr = curr.Prev;
+        }
     }
 
     public override string ToString()
